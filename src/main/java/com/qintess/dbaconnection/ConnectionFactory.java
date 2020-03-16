dbfa6fb62e5974a95a84718fa05f952c72b51b93
LeandroLeiteC <leandrolcosta213@gmail.com>
2020-03-16 13:23:00 -0300

    first commit

---
+++ src/main/java/com/qintess/dbaconnection/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.qintess.dbaconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public static Connection getConnection() {
+		String url = "jdbc:h2:mem:";
+		try{
+			return DriverManager.getConnection(url);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void closeConnection(Connection conn) {
+		try {
+			conn.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/qintess/dbaconnection/ConnectionTest.java
@@ -0,0 +1,11 @@
+package com.qintess.dbaconnection;
+
+import com.qintess.crud.crud;
+
+public class ConnectionTest {
+	public static void main(String[] args) {
+		
+		//método que cria a tabela, insere dados e printa na tela
+		crud.insert();
+	}
+}
